6d01e37bc8880f7072d7bf9f182b9d6cfa0606e9
Aseel <aswahaibi8@gmail.com>
2026-06-30 20:49:55 +0400

    Printing for the user and providing an output  using different data types + using convert

--- CsharpSession1/Program.cs
+++ CsharpSession1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /* //Printing for the user and providing an output  using different data types + using convert
+        Console.Write("Please enter your name: ");
+        string UserName = Console.ReadLine();
+        
+        Console.Write("Please enter your age: ");
+        int UserAge = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Please enter your Salary: ");
+        float UserSalary = float.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Hello  " + UserName + "  welcome to Spark To Code"); */
+        
     }
 }

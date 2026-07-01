a761174f89b4a3d49fd692d34b6d11e0c59332d2
Aseel <aswahaibi8@gmail.com>
2026-07-01 09:32:59 +0400

    task 1

--- tasks1/Program.cs
+++ tasks1/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
        //task1 
-        /*
+        
         string name = ("Sara");
         Console.Write("Name: " + name);
 
@@ -17,11 +17,12 @@ class Program
 
         bool student = true; 
         Console.Write("  student: " + student);
-        */ 
+        
         
         ///////////////////////////////////////////////////////
         
         //task2 
+        /*
         Console.Write(" enter the Rectangle length:  ");
         float length =  float.Parse(Console.ReadLine());
         
@@ -33,6 +34,12 @@ class Program
 
         Console.WriteLine(" The result of Area is :  " + area);
         Console.WriteLine(" The Perimeter is :  " + Perimeter);
+        */
+        
+        ///////////////////////////////////////////////////////////
+        
+        //task 3 
+        
 
 
 

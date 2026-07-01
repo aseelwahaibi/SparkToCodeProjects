bd859a9840c8b16a2d876816522c92b6a2eb104d
Aseel <aswahaibi8@gmail.com>
2026-07-01 08:44:24 +0400

    a

---
+++ tasks1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace tasks1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       //task1 
+        /*
+        string name = ("Sara");
+        Console.Write("Name: " + name);
+
+        int age = 21;
+        Console.Write("  Age: " + age);
+
+        double hight = 1.65;
+        Console.Write("  Hight: " + hight);
+
+        bool student = true; 
+        Console.Write("  student: " + student);
+        */ 
+        
+        ///////////////////////////////////////////////////////
+        
+        //task2 
+        
+        
+        
+
+
+
+
+    }
+}
